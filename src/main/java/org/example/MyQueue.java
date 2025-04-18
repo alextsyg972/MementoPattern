b7f6a890f0be0ca26d8dc74db9df81d2eaea4e06
alextsyg972 <tsyganovalexandr98@gmail.com>
2025-04-18 18:41:42 +1000

    change

--- src/main/java/org/example/MyQueue.java
+++ src/main/java/org/example/MyQueue.java
@@ -14,37 +14,33 @@ public class MyQueue {
         this.limit = limit;
     }
 
-    public void enqueue(int e) {
-        synchronized (this) {
-            while (queue.size() == limit) {
-                System.out.println("full");
-                try {
-                    wait();
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
+    public synchronized void enqueue(int e) {
+        while (queue.size() == limit) {
+            System.out.println("full");
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
             }
-            queue.add(e);
-            System.out.println("Element added: " + e);
-            notify();
         }
+        queue.add(e);
+        System.out.println("Element added: " + e);
+        notify();
     }
 
-    public int dequeue() {
-        synchronized (this) {
-            while (queue.isEmpty()) {
-                System.out.println("empty, wait");
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+    public synchronized int dequeue() {
+        while (queue.isEmpty()) {
+            System.out.println("empty, wait");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            int poll = queue.remove();
-            notify();
-            System.out.println("remove " + poll);
-            return poll;
         }
+        int poll = queue.remove();
+        notify();
+        System.out.println("remove " + poll);
+        return poll;
     }
 
 

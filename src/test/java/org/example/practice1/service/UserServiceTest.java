5075d8a6ac6f402c637120896864395d98649d1d
alextsyg972 <tsyganovalexandr98@gmail.com>
2025-04-24 21:53:11 +1000

    little fix

--- src/test/java/org/example/practice1/service/UserServiceTest.java
+++ src/test/java/org/example/practice1/service/UserServiceTest.java
@@ -4,9 +4,11 @@ import org.example.practice1.entity.User;
 import org.example.practice1.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;

f6fa7d8933391d9b16b2f2cc574317f1872ca21e
alextsyg972 <tsyganovalexandr98@gmail.com>
2025-05-02 02:11:01 +1000

    changes

--- src/main/java/org/example/springsecuritypractice3/controller/AdminController.java
+++ src/main/java/org/example/springsecuritypractice3/controller/AdminController.java
@@ -1,6 +1,5 @@
 package org.example.springsecuritypractice3.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/org/example/springsecuritypractice3/controller/AuthController.java
+++ src/main/java/org/example/springsecuritypractice3/controller/AuthController.java
@@ -1,7 +1,5 @@
 package org.example.springsecuritypractice3.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
